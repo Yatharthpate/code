e86c8735812d69139cacbe78697adda9f57ec82f
Yatharthpate <122634029+Yatharthpate@users.noreply.github.com>
2023-01-13 23:05:52 +0530

    Add files via upload

---
+++ c1.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int a;
+    printf("enter a number\n");
+    scanf("%d",&a);
+    if (a<10)
+    {
+        printf("Hello c\n");
+ 
+    }
+    else
+    {
+        printf("Hello programming\n");
+    }
+    return 0;
+}
---
+++ ifelse.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int a;
+    printf("enter number of years ");
+    scanf("%d",&a);
+    if ((a>3))
+    {
+        printf("Bonus=2500\n");
+    }
+    else
+    {
+        printf("no bonus");
+    }
+    return 0;
+
+}
